c858215c916ced6225237b303c4bf28752ef8c3d
Damien <damsmailspro@gmail.com>
2022-01-07 19:46:18 +0100

    installation de la map (leaflet) + quelques petites modif

--- src/components/Information/information.js
+++ src/components/Information/information.js
@@ -5,19 +5,19 @@ function Information() {
     <section className="information">
       <ul className="information__list">
         <li className="information__list--item">
-          <span className="information__list--item-title">IP Address</span>
+          <span className="information__list--item-title">Address IP</span>
           <p className="information__list--item-info">192.212.174.101</p>
         </li>
         <li className="information__list--item">
-          <span className="information__list--item-title">Location</span>
+          <span className="information__list--item-title">Emplacement</span>
           <p className="information__list--item-info">Brooklyn, NY 10001</p>
         </li>
         <li className="information__list--item">
-          <span className="information__list--item-title">Timezone</span>
+          <span className="information__list--item-title">Fuseau horaire</span>
           <p className="information__list--item-info">UTC -05:00</p>
         </li>
         <li className="information__list--item">
-          <span className="information__list--item-title">ISP</span>
+          <span className="information__list--item-title">FAI</span>
           <p className="information__list--item-info">SpaceX Starlink</p>
         </li>
       </ul>
--- src/components/Map/map.js
+++ src/components/Map/map.js
@@ -1,9 +1,27 @@
+import { MapContainer, TileLayer, Marker, Popup, ZoomControl } from 'react-leaflet';
+
 import './map.css';
 
 function Map() {
   return (
     <section className="map">
-      Map
+      <MapContainer 
+        center={[51.505, -0.09]} 
+        zoom={13} 
+        scrollWheelZoom={true}
+        zoomControl={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <ZoomControl position='bottomleft' />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            je suis un test dan un popup :)
+          </Popup>
+        </Marker>
+      </MapContainer>
     </section>
   );
 }
