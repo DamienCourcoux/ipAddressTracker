1bc8363ec77614d94b6ac63cdfa92ab050ddced6
Damien <damsmailspro@gmail.com>
2022-01-07 16:00:33 +0100

    css information

--- src/components/Information/information.js
+++ src/components/Information/information.js
@@ -3,7 +3,24 @@ import './information.css';
 function Information() {
   return (
     <section className="information">
-      Information
+      <ul className="information__list">
+        <li className="information__list--item">
+          <span className="information__list--item-title">IP Address</span>
+          <p className="information__list--item-info">192.212.174.101</p>
+        </li>
+        <li className="information__list--item">
+          <span className="information__list--item-title">Location</span>
+          <p className="information__list--item-info">Brooklyn, NY 10001</p>
+        </li>
+        <li className="information__list--item">
+          <span className="information__list--item-title">Timezone</span>
+          <p className="information__list--item-info">UTC -05:00</p>
+        </li>
+        <li className="information__list--item">
+          <span className="information__list--item-title">ISP</span>
+          <p className="information__list--item-info">SpaceX Starlink</p>
+        </li>
+      </ul>
     </section>
   );
 }
